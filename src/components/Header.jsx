4570dd657cf83c5b5af95bbeb5ac284f8fe67924
YaSidneva <yan-zublenk2008@narod.ru>
2026-04-30 12:42:30 +0200

    fix header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,9 @@ export default function Header() {
     return (
         <header>
             <div className="logo">
-                <img className="logoImg" src="../assets/logo.png" alt="logo"></img>
-                <div>Mindful</div></div>
+                <div className="logoImg"></div>
+                <div>Mindful</div>
+            </div>
 
             <nav>
                 <Link to="/">Home</Link>
