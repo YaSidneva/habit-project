04878678604c7a1530e542e73dcc15b36d99adb0
YaSidneva <yan-zublenk2008@narod.ru>
2026-05-04 11:55:00 +0200

    fix google autorizacion

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,11 +1,23 @@
 import { useGoogleLogin } from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
 import './AuthForm.css'
 
 export default function GoogleLoginButton() {
 
+    const navigate = useNavigate()
+
     const login = useGoogleLogin({
         onSuccess: (res) => {
             console.log(res)
+
+            // можно сохранить пользователя
+            localStorage.setItem('google_user', JSON.stringify(res))
+
+            // 🚀 редирект
+            navigate('/dashboard')
+        },
+        onError: () => {
+            console.log('Login Failed')
         }
     })
 
