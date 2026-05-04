48e07a067df1aee78f9ab5dab691bf04b0768091
YaSidneva <yan-zublenk2008@narod.ru>
2026-05-04 13:04:15 +0200

    fix google avtorizacion

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-        },
-    },
+
 })
 
